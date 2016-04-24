5ad7c7c63b04cbdd22d0be4fd3be39c0dfc242af
chwheeler7 <cgw897@gmail.com>
2016-04-23 21:09:58 -0500

    initial commit

--- Udemy2/ViewController.swift
+++ Udemy2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yankeesImg: UIImageView!
+    @IBOutlet weak var redsoxImg: UIImageView!
+    @IBOutlet weak var yankeesTxt: UIButton!
+    @IBOutlet weak var redsoxTxt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func clickYankees(sender: AnyObject) {
+        redsoxImg.hidden = true
+    }
+    
+    @IBAction func clickRedsox(sender: AnyObject) {
+        yankeesImg.hidden = true
+    }
 }
-
