ee773d74263403b1708ac5275c8b23bc3143fa77
chwheeler7 <cgw897@gmail.com>
2016-04-23 19:33:13 -0500

    Initial Commit

---
+++ Udemy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy2
+//
+//  Created by Chris Wheeler on 4/23/16.
+//  Copyright (c) 2016 Chris Wheeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Udemy2Tests/Udemy2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Udemy2Tests.swift
+//  Udemy2Tests
+//
+//  Created by Chris Wheeler on 4/23/16.
+//  Copyright (c) 2016 Chris Wheeler. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Udemy2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
